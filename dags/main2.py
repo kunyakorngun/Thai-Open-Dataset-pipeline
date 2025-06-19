e4087943d5f4d0ccc34a05c2e54ebe5bd55ad09e
kunyakorn <gun.gun2546@gmail.com>
2025-06-19 11:31:59 +0700

    mainpy : โหลดเข้าminioได้ทุกไฟล์หลือวนลูปทุกอันและเก็บlog

--- dags/main2.py
+++ dags/main2.py
@@ -15,9 +15,19 @@ MINIO_ACCESS_KEY = "minio"
 MINIO_SECRET_KEY = "minio123"
 BATCH_LIST_FILE = "/opt/airflow/dags/package_batches.json"
 
-# ปรับช่วงตรงนี้
-START_INDEX = 0
-END_INDEX = 5
+# total = 32473
+# batch_size = 50
+total = 100
+batch_size = 20
+
+# for start in range(0, total, batch_size):
+#     end = min(start + batch_size - 1, total - 1)
+#     # print(f"Trigger DAG with START_INDEX={start}, END_INDEX={end}")
+#     START_INDEX = start
+#     END_INDEX = end
+
+# START_INDEX = 0
+# END_INDEX = 32473
 
 @dag(
     dag_id="thai_open_dataset_pipeline_modular",
@@ -30,6 +40,11 @@ def pipeline():
 
     @task()
     def get_package_ids():
+        for start in range(0, total, batch_size):
+            end = min(start + batch_size - 1, total - 1)
+            START_INDEX = start
+            END_INDEX = end
+            
         with open(BATCH_LIST_FILE, "r", encoding="utf-8") as f:
             data = json.load(f)
         return data.get("result", [])[START_INDEX:END_INDEX + 1]
@@ -45,7 +60,8 @@ def pipeline():
         return {
             "package_id": package_id,
             "title": pkg.get("title", package_id),
-            "resources": pkg.get("resources", [])[:5]
+            "resources": pkg.get("resources", [])[:5], #ตัดเอาเฉพาะ 5 รายการแรก จาก list นั้นเท่านั้นตามที่กำหนดให้เอา resources ได้ไม่เกิน 5
+            #"last_modified": pkg.get("last_modified", []) #ถ้าไม่มีให้คืนเป็น list ว่าง
         }
 
     @task()
@@ -58,20 +74,34 @@ def pipeline():
         )
 
         title = pkg_data["title"].replace("/", "_").replace("\\", "_")
+        #สำหรับ download ไฟล์จาก resources
         for resource in pkg_data["resources"]:
-            original_url = resource.get("original_url")
-            if not original_url:
+            
+            if "original_url" in resource:
+                download_url = resource["original_url"]
+            elif "url" in resource:
+                download_url = resource["url"]
+            else:
+                download_url = None
+
+            if not download_url:
+                print(f"❌ No downloadable URL for resource: {resource}")
                 continue
 
+            #ไปเอา original_url หรือ url เพื่อมาโหลด มีอันไหนเอาอันนั้น
+            # original_url = resource.get("original_url") or resource.get("")
+            # # if not original_url:
+            # #     continue
+            
             try:
-                r = requests.get(original_url, timeout=20)
+                r = requests.get(download_url, timeout=20)
                 if r.status_code != 200:
-                    print(f"❌ Failed to download: {original_url}")
-                    continue
+                    print(f"❌ Failed to download: {download_url}")
+                    continue 
 
                 file_data = r.content
                 name = resource.get("name", resource["id"]).strip().replace("/", "_")
-                ext = os.path.splitext(original_url)[-1] or ".csv"
+                ext = os.path.splitext(download_url)[-1] or ".csv"
                 filename = f"{name}{ext}"
                 object_path = f"{title}/{filename}"
 
@@ -86,11 +116,13 @@ def pipeline():
                 print(f"✅ Uploaded: {object_path}")
 
             except Exception as e:
-                print(f"❌ Error downloading/uploading resource: {original_url} - {e}")
+                print(f"❌ Error downloading/uploading resource: {download_url} - {e}")
+
 
     # ==== DAG FLOW ====
     package_ids = get_package_ids()
     package_details = fetch_package_detail.expand(package_id=package_ids)
     download_and_upload_resources.expand(pkg_data=package_details)
+
 
 dag = pipeline()
